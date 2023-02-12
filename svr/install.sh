69f479e84242836f109b01b6e4fde414ccc3f22a
DergMcDergerson <p27182@gmail.com>
2023-02-12 08:57:22 -0500

    initial committtttt

---
+++ svr/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sh -c "apt install wireguard && wg genkey > /etc/wireguard/priv && wg pubkey < /etc/wireguard/priv > /etc/wireguard/pub"
+wget https://raw.githubusercontent.com/p27182/wg/main/svr/wg0.conf -O /etc/wireguard/wg0.conf
+echo "keys generated and saved in /etc/wireguard/ with wg0.conf - edit wg0.conf to add peer(s) IP and pub keys."
+echo "Remember to delete /etc/wireguard/priv and /etc/wireguard/pub"
+echo "Issue systemctl enable and start cmds after wg0.conf and peers configured."
