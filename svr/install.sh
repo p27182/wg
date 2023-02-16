37f79df08f98c51e69918a96561b72a4852e5704
DergMcDergerson <p27182@gmail.com>
2023-02-16 06:21:42 -0500

    added umask for correct perms

--- svr/install.sh
+++ svr/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sh -c "apt install wireguard && wg genkey > /etc/wireguard/priv && wg pubkey < /etc/wireguard/priv > /etc/wireguard/pub"
+sh -c "apt install wireguard && umask 077 && wg genkey > /etc/wireguard/priv && wg pubkey < /etc/wireguard/priv > /etc/wireguard/pub"
 wget https://raw.githubusercontent.com/p27182/wg/main/svr/wg0.conf -O /etc/wireguard/wg0.conf
 echo "keys generated and saved in /etc/wireguard/ with wg0.conf - edit wg0.conf to add peer(s) IP and pub keys."
 echo "Remember to delete /etc/wireguard/priv and /etc/wireguard/pub"
