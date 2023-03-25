c54ea16ae3dc997fd061af969924e065d546e32e
DergMcDergerson <p27182@gmail.com>
2023-03-25 11:58:56 -0400

    added comment for start and enable cmds

--- svr/install.sh
+++ svr/install.sh
@@ -4,3 +4,4 @@ wget https://raw.githubusercontent.com/p27182/wg/main/svr/wg0.conf -O /etc/wireg
 echo "keys generated and saved in /etc/wireguard/ with wg0.conf - edit wg0.conf to add peer(s) IP and pub keys."
 echo "Remember to delete /etc/wireguard/priv and /etc/wireguard/pub"
 echo "Issue systemctl enable and start cmds after wg0.conf and peers configured."
+echo "sudo systemctl enable wg-quick@wg0 && sudo systemctl start wg-quick@wg0"
